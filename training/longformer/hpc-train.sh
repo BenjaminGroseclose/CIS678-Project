6d4f4a11d92cdf5322e3d7d3924882fa93b03601
Ben Groseclose <ben.groseclose01@gmail.com>
2025-03-31 18:58:23 -0400

    Add Parameters

--- training/longformer/hpc-train.sh
+++ training/longformer/hpc-train.sh
@@ -25,7 +25,22 @@ mkdir -p $out_dir
 
 source /mnt/home/groseclb/CIS678-Project/venv./bin/activate
 
-python /mnt/home/groseclb/CIS678-Project/training/longformer/main.py
+python /mnt/home/groseclb/CIS678-Project/training/longformer/main.py \
+  --do_train \
+  --model_name_or_path $plm_dir \
+  --do_eval \
+  --train_file $train_file \
+  --validation_file $valid_file \
+  --max_seq_length 2048 \
+  --per_device_train_batch_size  2 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 5 \
+  --evaluation_strategy steps \
+  --eval_steps 1000 \
+  --overwrite_output_dir \
+  --gradient_accumulation_steps 8 \
+	--fp16
+  --output_dir $out_dir 2>&1 | tee $out_dir/log.train.$time
 
 deactivate
 
